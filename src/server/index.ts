03bacfa66697966bc837fdc60ff3c6a990b5eb93
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-06 23:14:34 -0400

    feat: put some endpoints in the server file

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,32 @@
 import http from 'http'
-import { OK } from '@/utils/status-codes.js'
+import { OK, NOT_FOUND } from '@/utils/status-codes.js'
 
-const server = http.createServer((_, res) => {
-  res.writeHead(OK, { 'Content-Type': 'application/json' })
-  const responseData = JSON.stringify({ message: 'Hello from Node.js! server' })
-
-  res.end(responseData)
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(OK, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Hello from Node.js! server' }))
+  } else if (req.url === '/test') {
+    res.writeHead(OK, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Hello from test endpoint!' }))
+  } else if (req.url === '/about') {
+    res.writeHead(OK, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Hello from about endpoint!' }))
+  } else if (req.url?.includes('user')) {
+    res.writeHead(OK, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Hello from user endpoint!' }))
+  } else if (req.url?.includes('messages')) {
+    res.write('<html>')
+    res.write('<body><h1>Hello from messages endpoint!</h1></body>')
+    res.write('</html>')
+  } else {
+    res.writeHead(NOT_FOUND, { 'Content-Type': 'application/json' })
+    res.end(
+      JSON.stringify({
+        error: 'Not Found',
+        message: `The route ${req.url} does not exists on the server`,
+      })
+    )
+  }
 })
 
 export default server
